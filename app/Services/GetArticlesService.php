57cacfc19b70a9086b83390fb0a395d397998907
Aliaksandr Tsishyn <aliaksandr.tsishyn@godigitalmg.com>
2023-12-01 18:50:22 +0300

    fix logic

--- app/Services/GetArticlesService.php
+++ app/Services/GetArticlesService.php
@@ -12,7 +12,7 @@ class GetArticlesService implements ArticlesServiceInterface
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         try {
-            for($i=1;$i<env('max_page');$i++) {
+            for($i=1;$i<=env('max_page');$i++) {
                 curl_setopt($ch, CURLOPT_URL, env('URL_articles').'?per_page=10&page='.$i);
                 $res  = curl_exec($ch);
                 $json = json_decode($res);
@@ -23,11 +23,12 @@ class GetArticlesService implements ArticlesServiceInterface
                 }
             }
 
+            $articles_collection=collect($array_articles);
+
         } catch (Exception $e){
             return collect([]);
         }
 
-        $articles_collection=collect($array_articles);
 
         return $articles_collection;
     }
